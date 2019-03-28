72c0bc3f53654e252553779647880c116e7ea131
programer10 <programer10@ucione.local>
2019-03-28 18:56:48 +0100

    vjezbe2

--- 2.01_vjezba/Controllers/KontekstController.cs
+++ 2.01_vjezba/Controllers/KontekstController.cs
@@ -29,5 +29,22 @@ namespace _2._01_vjezba.Controllers
                 return string.Format("<p>Ime i prezime iz query stringa su: {0}</p>", textSaStilom);
             }
         }
+        public string RoutePodaci()
+        {
+            try
+            {
+                string kontroler = RouteData.Values["controller"].ToString();
+                string akcijskaMetoda = RouteData.Values["action"].ToString();
+                string parametarId = RouteData.Values["id"].ToString();
+                return "<h1>Route podaci:</h1>" +
+                    "Kontroler: " + kontroler + "<br />" +
+                    "Metoda: " + akcijskaMetoda + "<br />" +
+                    "Parametar id: " + parametarId + "<br />";
+            }
+            catch (Exception ex)
+            {
+                return "Došlo je do pogreške: " + ex.Message;
+            }
+        }
     }
 }
---
+++ HelloWorld/Controllers/GeneratorIzlazaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class GeneratorIzlazaController : Controller
+    {
+        // GET: GeneratorIzlaza/PopisKosarice
+        public ActionResult PopisKosarice()
+        {
+            return View();
+        }
+        // GET: GeneratorIzlaza/ListaArtikala
+        public ActionResult ListaArtikala()
+        {
+
+        } 
+    }
+}
