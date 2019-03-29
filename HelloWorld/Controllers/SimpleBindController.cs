dfbab8cf13c7b3fd15d62d7a2dfb06315476316b
Rihard Lagundžija <rihardla@gmail.com>
2019-03-29 17:54:46 +0100

    binding model

---
+++ HelloWorld/Controllers/SimpleBindController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class SimpleBindController : Controller
+    {
+        // GET: SimpleBind
+        public ViewResult SimpleBindMetoda()
+        {
+            return View("SimpleBindMetoda");
+        }
+        // POST: SimpleBind
+        [HttpPost]
+        public ViewResult SimpleBindMetoda(string ime)
+        {
+            string pozdrav = "Pozdrav " + ime + "!";
+            return View("SimpleBindMetoda", (object)pozdrav);
+        }
+    }
+}
