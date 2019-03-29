affa2d9729c79b5e4afdb0123b2aee94d67310ef
Rihard Lagundžija <rihardla@gmail.com>
2019-03-29 18:40:32 +0100

    Moodel post

---
+++ HelloWorld/Controllers/OsobeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class OsobeController : Controller
+    {
+        // GET: Osoba
+        public ActionResult PopuniOsobu()
+        {
+            return View();
+        }
+        // POST: Osoba/PrikaziOsobu
+        [HttpPost]
+        public ViewResult PrikaziOsobu(Osoba osoba)
+        {
+            return View(osoba);
+        }
+    }
+}
