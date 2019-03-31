682487e70351e8cab789e2b1c44c181dfc61f640
rihard11 <rihardla@gmail.com>
2019-03-31 18:17:44 +0200

    Zadaca_4.2._vj

---
+++ Zadaca_4.1._vj/Controllers/NaruciArtikalController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadaca_4._1._vj.Models;
+
+namespace Zadaca_4._1._vj.Controllers
+{
+    public class NaruciArtikalController : Controller
+    {
+        // GET: NaruciArtikal
+        public ViewResult NaruciArtikal()
+        {
+            return View(new Artikl());
+        }
+
+        // POST
+        [HttpPost]
+        public ViewResult NaruciArtikal(Artikl artikal)
+        {
+            if (artikal.Kolicina > 10)
+            {
+                ViewBag.Poruka = "Nema dovoljno " + artikal.Naziv + " na skladištu!";
+                return View(artikal);
+            }
+            else
+            {
+                ViewBag.Poruka = "Naručeno je " +
+                    artikal.Kolicina +
+                    " komad/a " +
+                    artikal.Naziv +
+                    " sa ukupnom cijenom " +
+                    artikal.Cijena * artikal.Kolicina +
+                    " HRK.";
+                return View(artikal);
+            }
+        }
+    }
+}
---
+++ Zadaca_4.1._vj/Models/Artikl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zadaca_4._1._vj.Models
+{
+    public class Artikl
+    {
+        public string Kategorija { get; set; }
+        public string Naziv { get; set; }
+        public decimal Cijena { get; set; }
+        public int Kolicina { get; set; }
+    }
+}
