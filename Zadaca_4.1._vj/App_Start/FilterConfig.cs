a413ef639e07559728ef3bd9620fd05e0f8178d1
rihard11 <rihardla@gmail.com>
2019-03-31 17:54:35 +0200

    Zadaca_4.1._vj

---
+++ Zadaca_4.1._vj/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Zadaca_4._1._vj
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Zadaca_4.1._vj/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zadaca_4._1._vj.MvcApplication" Language="C#" %>
