773e2dd908bd9a1f03ce64049be578100fc4d384
Rihard Lagundžija <rihardla@gmail.com>
2019-04-03 18:17:24 +0200

    helperi forme non-strongli typed

---
+++ Zadaca_4.1._vj/Controllers/HtmlHelperiController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadaca_4._1._vj.Models;
+
+namespace Zadaca_4._1._vj.Controllers
+{
+    public class HtmlHelperiController : Controller
+    {
+        // GET: HtmlHelperi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private string[] mjesta = new string[]
+        {
+            "Split", "Osijek", "Zadar", "Rijeka"
+        };
+
+        public ViewResult FormHelper()
+        {
+            ViewBag.Mjesta = this.mjesta;
+            return View(new Osoba());
+        }
+        [HttpPost]
+        public ViewResult FormHelper(Osoba osoba)
+        {
+            ViewBag.Mjesta = this.mjesta;
+            ViewBag.Poruka = "BlaBla";
+            return View(new Osoba());
+        }
+    }
+}
---
+++ Zadaca_4.1._vj/Models/Osoba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zadaca_4._1._vj.Models
+{
+    public class Osoba
+    {
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public string Email { get; set; }
+        public DateTime? DatumRodjenja { get; set; }
+        public string MjestoStanovanje { get; set; }
+        public bool Zaposlen { get; set; }
+        public string Spol { get; set; }
+
+    }
+}
