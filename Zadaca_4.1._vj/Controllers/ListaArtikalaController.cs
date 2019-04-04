687906a766272e4692ba4efe4d674dfe069e5227
rihard11 <rihardla@gmail.com>
2019-04-04 14:49:24 +0200

    Zadaca_6.1.1

---
+++ Zadaca_4.1._vj/Controllers/ListaArtikalaController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zadaca_4._1._vj.Models;
+
+namespace Zadaca_4._1._vj.Controllers
+{
+    public class ListaArtikalaController : Controller
+    {
+        // GET: ListaArtikala/UnesiArtikl
+        public ActionResult UnesiArtikl()
+        {
+            ViewBag.Kategorije = new string[] { "Sport", "Glazba", "Tehnika" };
+            return View(new Artikl());
+        }
+
+        // POST: ListaArtikala/DodajArtikl 
+        [HttpPost]
+        public ActionResult DodajArtikl(Artikl artikl)
+        {
+            if (Session["Artikli"] != null)
+            {
+                List<Artikl> artikli = (List<Artikl>)Session["Artikli"];
+                artikli.Add(artikl);
+                Session["Artikli"] = artikli;
+            }
+            else
+            {
+                List<Artikl> artikli = new List<Artikl>();
+                artikli.Add(artikl);
+                Session["Artikli"] = artikli;
+            }
+            return View(Session["Artikli"]);
+        }
+    }
+}
