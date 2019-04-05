84a28a5558a9e6971df71082ba72a826868f159a
programer10 <programer10@ucione.local>
2019-04-05 16:28:38 +0200

    Validacije

---
+++ Validacije/Models/Racun.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Validacije.Models
+{
+    public class Racun
+    {
+        public string BrojRacuna { get; set; }
+        public string Datum { get; set; }
+        public string Zaposlenik { get; set; }
+        public string Kupac { get; set; }
+        public string Cijana { get; set; }
+    }
+}
